7f12a5679f0462d924e4317a897c1a3e1eab7963
Kirill <kirill.petersburg@gmail.com>
2020-09-19 02:55:10 +0300

    preamble was used

--- rk_pd_4d.py
+++ rk_pd_4d.py
@@ -1,183 +1,167 @@
 # Use OpenCL For Prince Dormand Calculations (Using PyOpenCL Arrays and Elementwise)
 
 rungeKutta = '''
+inline double RungeKutta(double4* y_next_time, double4* p_y_current_time, double current_time, double time_delta)
+{
     const double two_thirds = 2.0 / 3.0;
     const double one_seventwoninths = 1.0 / 729.0;
     const double one_twoninesevenzero = 1.0 / 2970.0;
     const double one_twofivetwozero = 1.0 / 2520.0;
     const double one_fiveninefourzero = 1.0 / 5940.0;
 
-    # define RungeKutta(y_next_time, y_current_time, current_time, time_delta, error_runge_kutta) \
-    do \
-    { \
-        double4 k1, k2, k3, k4, k5, k6; \
-        double4 y_t_runge_kutta; \
-    \
-        double h5 = 0.2 * (time_delta); \
-    \
-        derivedFn(k1, (y_current_time), (current_time));                            y_t_runge_kutta = (y_current_time) + h5 * k1; \
-        derivedFn(k2, y_t_runge_kutta, (current_time) + h5);                        y_t_runge_kutta = (y_current_time) + (time_delta) * (0.075 * k1 + 0.2250 * k2); \
-        derivedFn(k3, y_t_runge_kutta, (current_time) + 0.3 * (time_delta));        y_t_runge_kutta = (y_current_time) + (time_delta) * (0.300 * k1 - 0.9000 * k2 + 1.2000 * k3); \
-        derivedFn(k4, y_t_runge_kutta, (current_time) + 0.6 * (time_delta));        y_t_runge_kutta = (y_current_time) + (time_delta) * (226.0 * k1 - 675.00 * k2 + 880.00 * k3 + 55.0000 * k4) * one_seventwoninths; \
-        derivedFn(k5, y_t_runge_kutta, (current_time) + two_thirds * (time_delta)); y_t_runge_kutta = (y_current_time) + (time_delta) * (-1991 * k1 + 7425.0 * k2 - 2660.0 * k3 - 10010.0 * k4 + 10206.0 * k5) * one_twoninesevenzero; \
-        derivedFn(k6, y_t_runge_kutta, (current_time) + (time_delta)); \
-    \
-        y_next_time = (y_current_time) + (time_delta) * (341.0 * k1 + 3800.0 * k3 - 7975.0 * k4 + 9477.0 * k5 + 297.0 * k6) * one_fiveninefourzero; \
-    \
-        error_runge_kutta = length(one_twofivetwozero * (77.0 * k1 - 400.0 * k3 + 1925.0 * k4 - 1701.0 * k5 + 99.0 * k6)); \
-    } \
-    while (0)
+    double4 k1, k2, k3, k4, k5, k6;
+    double4 y_t_runge_kutta;
 
+    double h5 = 0.2 * time_delta;
+
+    k1 = derivedFn(p_y_current_time, current_time);                           y_t_runge_kutta = *p_y_current_time + h5 * k1;
+    k2 = derivedFn(&y_t_runge_kutta, current_time + h5);                      y_t_runge_kutta = *p_y_current_time + time_delta * (0.075 * k1 + 0.2250 * k2);
+    k3 = derivedFn(&y_t_runge_kutta, current_time + 0.3 * time_delta);        y_t_runge_kutta = *p_y_current_time + time_delta * (0.300 * k1 - 0.9000 * k2 + 1.2000 * k3);
+    k4 = derivedFn(&y_t_runge_kutta, current_time + 0.6 * time_delta);        y_t_runge_kutta = *p_y_current_time + time_delta * (226.0 * k1 - 675.00 * k2 + 880.00 * k3 + 55.0000 * k4) * one_seventwoninths;
+    k5 = derivedFn(&y_t_runge_kutta, current_time + two_thirds * time_delta); y_t_runge_kutta = *p_y_current_time + time_delta * (-1991 * k1 + 7425.0 * k2 - 2660.0 * k3 - 10010.0 * k4 + 10206.0 * k5) * one_twoninesevenzero;
+    k6 = derivedFn(&y_t_runge_kutta, current_time + time_delta);
+
+    *y_next_time = *p_y_current_time + time_delta * (341.0 * k1 + 3800.0 * k3 - 7975.0 * k4 + 9477.0 * k5 + 297.0 * k6) * one_fiveninefourzero;
+
+    return length(one_twofivetwozero * (77.0 * k1 - 400.0 * k3 + 1925.0 * k4 - 1701.0 * k5 + 99.0 * k6));
+}
 '''
 
 princeDormand = '''
-    # define PrinceDormand(y_next_time, y_current_time, current_time, t_next_time, time_delta, _updated_time_delta, tolerance, _pd_result) \
-    do \
-    { \
-        double tail = t_next_time - (current_time); \
-    \
-        if (tail < 0 || (time_delta) <= 0.0) \
-        { \
-            _pd_result = pd_failed_wrong_step; \
-    \
-            break; \
-        } \
-    \
-        _pd_result = pd_success; \
-    \
-        if (tail == 0) \
-        { \
-            break; \
-        } \
-    \
-        double4 temp_y; \
-        double4 temp_yt; \
-    \
-        double temp_time_delta = (time_delta); \
-    \
-        _updated_time_delta = temp_time_delta; \
-    \
-        double temp_tolerance = (tolerance) / tail; \
-    \
-        double pd_current_time = (current_time); \
-    \
-        y_next_time = (y_current_time); \
-    \
-        temp_y = (y_current_time); \
-    \
-        bool last_interval = false; \
-    \
-        do \
-        { \
-            if (temp_time_delta > tail) { last_interval = true; temp_time_delta = tail; } else if (1.5 * temp_time_delta > tail) temp_time_delta *= 0.5; \
-    \
-            double scale = 1.0; \
-    \
-            int attempts_left = pd_attempts; \
-    \
-            while (--attempts_left >= 0) \
-            { \
-                double pd_rk_error = 0.0; \
-    \
-                RungeKutta(temp_yt, temp_y, pd_current_time, temp_time_delta, pd_rk_error); \
-    \
-                if (pd_rk_error == 0.0) \
-                { \
-                    scale = pd_max_scale_factor; \
-    \
-                    break; \
-                } \
-    \
-                double function_module = length(temp_y); \
-    \
-                double function_tolerance = (function_module == 0.0) ? temp_tolerance : function_module; \
-    \
-                scale = min(max(0.8 * sqrt(sqrt(temp_tolerance * function_tolerance / pd_rk_error)), pd_min_scale_factor), pd_max_scale_factor); \
-    \
-                if (pd_rk_error < (temp_tolerance * function_tolerance)) break; \
-    \
-                temp_time_delta *= scale; \
-    \
-                if (temp_time_delta > tail) { temp_time_delta = tail; } else if (1.5 * temp_time_delta > tail) temp_time_delta *= 0.5; \
-            } \
-    \
-            if (attempts_left <= 0) \
-            { \
-                    _updated_time_delta = temp_time_delta * scale; \
-    \
-                    _pd_result = pd_failed_too_many_function_calls; \
-    \
-                    break; \
-            } \
-    \
-            temp_y = temp_yt; \
-    \
-            pd_current_time += temp_time_delta; temp_time_delta *= scale; _updated_time_delta = temp_time_delta; \
-        } \
-        while ((tail = t_next_time - pd_current_time) > 0.0 && !last_interval && (_pd_result == pd_success)); \
-    \
-        if ((_pd_result == pd_success)) {y_next_time = temp_yt;} \
-    } \
-    while (0)
+# define pd_attempts (32)
+# define pd_min_scale_factor (0.0625)
+# define pd_max_scale_factor (16.0)
 
+# define pd_success (0)
+# define pd_failed_too_many_function_calls (-1)
+# define pd_failed_wrong_step (-2)
+
+inline int PrinceDormand(__global double4* p_y_next_time, double4* p_y_current_time, double current_time, double t_next_time, double* p_updated_time_delta, double tolerance)
+{
+    double tail = t_next_time - current_time;
+
+    if (tail < 0 || *p_updated_time_delta <= 0.0)
+    {
+        return pd_failed_wrong_step;
+    }
+
+    if (tail == 0)
+    {
+        return pd_success;
+    }
+
+    double4 temp_y;
+    double4 temp_yt;
+
+    double temp_time_delta = *p_updated_time_delta;
+
+     double temp_tolerance = tolerance / tail;
+
+    double pd_current_time = current_time;
+
+    *p_y_next_time = *p_y_current_time;
+
+    temp_y = *p_y_current_time;
+
+    bool last_interval = false;
+
+    int pd_result = pd_success;
+
+    do
+    {
+        if (temp_time_delta > tail) { last_interval = true; temp_time_delta = tail; } else if (1.5 * temp_time_delta > tail) temp_time_delta *= 0.5;
+
+        double scale = 1.0;
+
+        int attempts_left = pd_attempts;
+
+        while (--attempts_left >= 0)
+        {
+            double pd_rk_error = RungeKutta(&temp_yt, &temp_y, pd_current_time, temp_time_delta);
+
+            if (pd_rk_error == 0.0)
+            {
+                scale = pd_max_scale_factor;
+
+                break;
+            }
+
+            double function_module = length(temp_y);
+
+            double function_tolerance = (function_module == 0.0) ? temp_tolerance : function_module;
+
+            scale = min(max(0.8 * sqrt(sqrt(temp_tolerance * function_tolerance / pd_rk_error)), pd_min_scale_factor), pd_max_scale_factor);
+
+            if (pd_rk_error < (temp_tolerance * function_tolerance)) break;
+
+            temp_time_delta *= scale;
+
+            if (temp_time_delta > tail) { temp_time_delta = tail; } else if (1.5 * temp_time_delta > tail) temp_time_delta *= 0.5;
+        }
+
+        if (attempts_left <= 0)
+        {
+                *p_updated_time_delta = temp_time_delta * scale;
+
+                pd_result = pd_failed_too_many_function_calls;
+
+                break;
+        }
+
+        temp_y = temp_yt;
+
+        pd_current_time += temp_time_delta; temp_time_delta *= scale; *p_updated_time_delta = temp_time_delta;
+    }
+    while ((tail = t_next_time - pd_current_time) > 0.0 && !last_interval && (pd_result == pd_success));
+
+    if (pd_result == pd_success)
+    {
+        *p_y_next_time = temp_yt;
+    }
+
+    return pd_result;
+}
 '''
 
 ode_arguments = 'double t0, double t, double dt, double minimum_dt, int mesh_size, double tolerance, double4 *initializing_parameters, double4 *y, int *pd_result'
 
 ode_operation = '''
-    # define pd_attempts (32)
-    # define pd_min_scale_factor (0.0625)
-    # define pd_max_scale_factor (16.0)
-
-    # define pd_success (0)
-    # define pd_failed_too_many_function_calls (-1)
-    # define pd_failed_wrong_step (-2)
-
     pd_result[i] = pd_success;
     double updated_time_delta = dt;
 
-    MakeInitialValues(y[i], initializing_parameters[i]);
+    y[i] = MakeInitialValues(&initializing_parameters[i]);
 
     double mesh_time_interval = (t - t0) / mesh_size;
 
     for (int mesh_index = 0; mesh_index < mesh_size; mesh_index++)
     {
-        if (pd_result[i] != pd_success) break;
-
         double4 y_initial = y[i];
 
         double mesh_t0 = t0 + mesh_index * mesh_time_interval;
         double mesh_t  = mesh_t0 + mesh_time_interval;
 
-        PrinceDormand(y[i], y_initial,
-                      (mesh_t0), (mesh_t),
-                      (updated_time_delta), updated_time_delta,
-                      tolerance, pd_result[i]);
+        pd_result[i] = PrinceDormand(&y[i], &y_initial, mesh_t0, mesh_t, &updated_time_delta, tolerance);
+
+        if (pd_result[i] != pd_success) break;
 
         if (updated_time_delta < minimum_dt) updated_time_delta = minimum_dt;
     }
     '''
 
 derivedFnCoulomb = '''
-    # define derivedFn(k, Y, _t) \
-    do \
-    { \
-        double r = length(Y.xy); \
-    \
-        k.xy = Y.zw; k.zw = Y.xy / (r * r * r); \
-    } \
-    while (0)
+inline double4 derivedFn(double4* Y, double t)
+{
+    double r = length(Y->xy);
 
+    return (double4)(Y->zw, Y->xy / (r * r * r));
+}
 '''
 
 makeInitialValuesFromInitializingParametersCoulomb = '''
-    #define MakeInitialValues(_y0, _initializing_parameter) \
-    do \
-    { \
-        _y0 = _initializing_parameter; \
-    } \
-    while (0)
-
+inline double4 MakeInitialValues(__global double4 *initializing_parameter)
+{
+    return *initializing_parameter;
+}
 '''
 
 
@@ -240,7 +224,9 @@ if __name__ == '__main__':
     #  - Name: the fuction name as which the kernel is compiled
 
     ode = cl.elementwise.ElementwiseKernel(
-        context, ode_arguments, ('{}' * 5).format(makeInitialValuesFromInitializingParametersCoulomb, derivedFnCoulomb, rungeKutta, princeDormand, ode_operation), 'ode')
+        context, ode_arguments, ode_operation,
+        name='ode',
+        preamble=f'{makeInitialValuesFromInitializingParametersCoulomb}{derivedFnCoulomb}{rungeKutta}{princeDormand}')
 
     # Call the elementwise kernel
     ode(0.0, 4096.0, 409.6, 0.4096, 32, 1.0e-14,
--- test_pytest.py
+++ test_pytest.py
@@ -30,7 +30,7 @@ def test_parameters_size():
 )
 def test_derivedFnCoulomb(initials, expected):
     sut = cl.elementwise.ElementwiseKernel(
-        context, 'double4 *k, double4 *y, double t', '{}{}'.format(rk_pd_4d.derivedFnCoulomb, 'derivedFn(k[0], (y[0]), (t))'), 'sut')
+        context, 'double4 *k, double4 *y, double t', 'double4 temp_y = y[0]; k[0] = derivedFn(&temp_y, t)', name='sut', preamble=rk_pd_4d.derivedFnCoulomb)
 
     y = cl_array.to_device(queue, initials)
     k = cl_array.empty_like(y)
@@ -49,23 +49,22 @@ def test_derivedFnCoulomb(initials, expected):
          numpy.array([cltypes.make_double4(2.0, 2.0, 2.0, 2.0)]), 1e-18, 1e-18, numpy.array([numpy.double(0.0)])),
         (numpy.array([cltypes.make_double4(1.0, 1.0, 1.0, 1.0)]), 0.0, 1.0, '1.0, -1.0, 1.0, -1.0',
          numpy.array([cltypes.make_double4(2.0, 0.0, 2.0, 0.0)]), 1e-18, 1e-18, numpy.array([numpy.double(0.0)])),
-        (numpy.array([cltypes.make_double4(0.0, 1.0, 0.0, 1.0)]), 0.0, 1e-2, 'Y.z, -Y.w, Y.x, -Y.y',
+        (numpy.array([cltypes.make_double4(0.0, 1.0, 0.0, 1.0)]), 0.0, 1e-2, 'Y->z, -Y->w, Y->x, -Y->y',
          numpy.array([cltypes.make_double4(numpy.sin(1e-2), numpy.cos(1e-2), numpy.sin(1e-2), numpy.cos(1e-2))]), 2e-2, 2e-10, numpy.array([numpy.double(1e-11)])),
     ]
 )
 def test_RungeKutta(initials, t0, t1, derived_function, expected, absolute_error, relative_error, expected_error_runge_kutta):
-    sut_derivedFn = '''
-        # define derivedFn(k, Y, _t) \
-        do \
-        {{ \
-            k = (double4)({}); \
-        }} \
-        while (0)
-
-    '''.format(derived_function)
+    sut_derivedFn = f'''double4 derivedFn(double4* Y, double t)
+    {{
+        return (double4)({derived_function});
+    }}'''
 
     sut = cl.elementwise.ElementwiseKernel(
-        context, 'double4 *y, double4 *y0, double t, double dt, double* error_runge_kutta', '{}{}{}'.format(sut_derivedFn, rk_pd_4d.rungeKutta, 'RungeKutta(y[0], y0[0], t, dt, *error_runge_kutta)'), 'sut')
+        context,
+        'double4 *y, double4 *y0, double t, double dt, double* error_runge_kutta',
+        'double4 temp_y = y[0]; double4 temp_y0 = y0[0]; *error_runge_kutta = RungeKutta(&temp_y, &temp_y0, t, dt); y[0] = temp_y',
+        name='sut',
+        preamble=f'{sut_derivedFn}{rk_pd_4d.rungeKutta}')
 
     y0 = cl_array.to_device(queue, initials)
     y = cl_array.empty_like(y0)
